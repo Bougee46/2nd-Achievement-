1f8fa7c9761ac26b5576836df5df8af6a225317f
Bougee46 <b.enehizena@yahoo.com>
2022-12-23 15:19:24 +0100

    Updated Files

--- js/script.js
+++ js/script.js
@@ -3,6 +3,24 @@
 carrer foundry*/
 
 let myName = 'Bryan';
-document.write(myName);
-myName = 'carla';
-document.write(myName);
+// document.write(myName);
+myName = 'Carla';
+// document.write(myName);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition)
+
+let size1 = 100;
+let dubbleSize = 2 * size1;
+document.write(dubbleSize)
+
+let group = 10;
+let cars = 3;
+let howMany = group / cars;
+document.write(howMany)
+
+let size = 400;
+let doubleSize = size * 2;
+document.write(doubleSize) // Why dose console.log dont work with the code?
+let minSize = (doubleSize * 2) - (size/2);
+document.write(minSize)
