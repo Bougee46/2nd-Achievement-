72ffc5cb9d9b1e5a47d9fdfaf5fa8b7821e336cd
Bougee46 <b.enehizena@yahoo.com>
2022-12-28 12:19:38 +0100

    Started Pokemon app

--- js/script.js
+++ js/script.js
@@ -1,32 +1,52 @@
-//Simple app
-/*first achivment 
-carrer foundry*/
+// //Simple app
+// /*first achivment 
+// carrer foundry*/
 
-alert('Hello World!');
+// alert('Hello World!');
 
-let favoritFruit = 'Mango';
-document.write(favoritFruit);
+// let favoritFruit = 'Mango';
+// document.write(favoritFruit);
 
 
-let myName = 'Bryan';
-// document.write(myName);
-myName = 'Carla';
-// document.write(myName);
+// let myName = 'Bryan';
+// // document.write(myName);
+// myName = 'Carla';
+// // document.write(myName);
 
-let simpleAddition = 2 + 2;
-document.write(simpleAddition)
+// let simpleAddition = 2 + 2;
+// document.write(simpleAddition)
 
-let size1 = 100;
-let dubbleSize = 2 * size1;
-document.write(dubbleSize)
+// let size1 = 100;
+// let dubbleSize = 2 * size1;
+// document.write(dubbleSize)
 
-let group = 10;
-let cars = 3;
-let howMany = group / cars;
-document.write(howMany)
+// let group = 10;
+// let cars = 3;
+// let howMany = group / cars;
+// document.write(howMany)
 
-let size = 400;
-let doubleSize = size * 2;
-document.write(doubleSize) // Why dose console.log dont work with the code?
-let minSize = (doubleSize * 2) - (size/2);
-document.write(minSize)
+// let size = 400;
+// let doubleSize = size * 2;
+// document.write(doubleSize) // Why dose console.log dont work with the code?
+// let minSize = (doubleSize * 2) - (size/2);
+// document.write(minSize)
+
+
+// //let name = 'John';
+// let welcomeMessage1 = "Hello, I'm";
+// let welcomeMessage2 = 'and I say to you: "Hello!"';
+// //console.log(welcomeMessage1 + ' ' + name + ' ' + welcomeMessage2);
+
+// // Template Literals
+// let name = 'John';
+// let age = 32;
+// let message = 'Hello!';
+// console.log(`${message} my name is ${name} and I'm ${age} Years old!`)
+
+
+let pokemonList = [
+    {name: "Venusaur", height: '6', type: ['grass', 'poison'] },
+    {name: "Charizard", height: '5', type: ['fire', 'flying'] },
+    {name: "Blastoise", height: '5', type: ['water, water'] },
+    {name: "Onix", height: '28', type: ['Rock', 'ground'] }
+ ]
